8961c086f7bbb6d7d8660ee2cf366b79188846ca
Francesco Calì <califrancesco98@gmail.com>
2024-08-27 19:50:51 +0200

    Refactoring pcController

--- src/main/java/com/betacom/eCommerce/classes/controller/PcController.java
+++ src/main/java/com/betacom/eCommerce/classes/controller/PcController.java
@@ -3,6 +3,7 @@ package com.betacom.eCommerce.classes.controller;
 import com.betacom.eCommerce.classes.dto.request.PcRequest;
 import com.betacom.eCommerce.classes.dto.view.PcView;
 import com.betacom.eCommerce.classes.dto.view.ProductView;
+import com.betacom.eCommerce.classes.dto.view.PsuView;
 import com.betacom.eCommerce.classes.response.Response;
 import com.betacom.eCommerce.classes.response.ResponseBase;
 import com.betacom.eCommerce.classes.response.ResponseObject;
@@ -24,6 +25,19 @@ public class PcController {
         try{
             service.create(req);
         }catch (Exception e){
+            resp.setRc(false);
+            resp.setMsg(e.getMessage());
+        }
+        return resp;
+    }
+
+    @GetMapping("/list")
+    public Response<PcView> listAll(){
+        Response<PcView> resp = new Response<PcView>();
+        resp.setRc (true);
+        try{
+            resp.setDati (service.list());
+        }catch(Exception e){
             resp.setRc(false);
             resp.setMsg(e.getMessage());
         }
